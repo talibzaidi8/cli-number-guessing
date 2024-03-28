fcb91eb49745b51ee87a0d9d7ed1423c0e2470d9
talib zaidi <sajidzaidi416@gmail.com>
2024-03-28 22:19:43 +0500

    create number guessing game

---
+++ index.ts
@@ -0,0 +1,24 @@
+#! usr/bin/env code
+
+import inquirer from "inquirer";
+
+
+
+const randomNumber = Math.floor(Math.random()*6 + 1);
+
+
+const answer = await inquirer.prompt([
+   { 
+    name:"userGuessednumber",
+    type:"number",
+    message:"please guess a number between 1 to 6:", 
+
+   }
+
+]);
+if(answer.userGuessednumber===randomNumber){
+console.log("Congtratulation! you guessed right number.");
+}else{
+console.log("you guessed wrong number");
+}
+
