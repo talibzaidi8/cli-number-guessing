39f847830f06bd2a3434f52d76f665401528b7db
talib zaidi <sajidzaidi416@gmail.com>
2024-03-28 22:27:57 +0500

    added a ,

--- index.ts
+++ index.ts
@@ -13,12 +13,12 @@ const answer = await inquirer.prompt([
     type:"number",
     message:"please guess a number between 1 to 6:", 
 
-   }
+   },
 
 ]);
 if(answer.userGuessednumber===randomNumber){
 console.log("Congtratulation! you guessed right number.");
 }else{
 console.log("you guessed wrong number");
-}
+};
 
