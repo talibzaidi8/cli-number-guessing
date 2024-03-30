8ccaa485b16171b218db1b86c9bf3ae43e24e491
talib zaidi <sajidzaidi416@gmail.com>
2024-03-30 09:57:33 +0500

    correct the erors

--- index.ts
+++ index.ts
@@ -2,23 +2,17 @@
 
 import inquirer from "inquirer";
 
-
-
-const randomNumber = Math.floor(Math.random()*6 + 1);
-
+const randomNumber = Math.floor(Math.random() * 6 + 1);
 
 const answer = await inquirer.prompt([
-   { 
-    name:"userGuessednumber",
-    type:"number",
-    message:"please guess a number between 1 to 6:", 
-
-   },
-
+  {
+    name: "userGuessednumber",
+    type: "number",
+    message: "please guess a number between 1 to 6:",
+  },
 ]);
-if(answer.userGuessednumber===randomNumber){
-console.log("Congtratulation! you guessed right number.");
-}else{
-console.log("you guessed wrong number");
+if (answer.userGuessednumber === randomNumber) {
+  console.log("Congtratulation! you guessed right number.");
+} else {
+  console.log("you guessed wrong number");
 }
-
